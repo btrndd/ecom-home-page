e7d4d5373c12da6085184ce86271b086603c7325
Bruno Trindade <brunotrndd@gmail.com>
2021-09-12 19:41:12 -0300

    finalização do slideshow

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -3,37 +3,29 @@ import 'react-slideshow-image/dist/styles.css'
 import { Slide } from 'react-slideshow-image';
 import './Slideshow.css';
 import slide1 from '../img/slide_1.jpg';
-import slide2 from '../img/slide_3.jpg';
-import slide3 from '../img/slide_4.jpg';
 import { FaCircle } from 'react-icons/fa';
 
 class Slideshow extends React.Component {
   render() {
-    const slideImages = [slide1, slide2, slide3];
+    const slideImages = [slide1];
     const properties = {
       indicators: true,
       scale: 0.01,
       // eslint-disable-next-line 
       indicators: i => (<div className="indicator"><FaCircle /></div>),
       arrows: false,
+      infinite: false,
+      autoplay: false,
     }
-
     return (
       <div className="slideshow">
         <Slide {...properties}>
           <div className="each-slide">
             <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-              <span>Slide 1</span>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-              <span>Slide 2</span>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-              <span>Slide 3</span>
+              <span>
+                <h4>Olá, você quer fazer entrar de cabeça em desenvolvimento?</h4>
+                <h2>front-end • back-end • mobile</h2>
+              </span>
             </div>
           </div>
         </Slide>
