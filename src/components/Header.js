bb87322d241b0945570663d4dc8a02bbb1235117
Bruno Trindade <brunotrndd@gmail.com>
2021-09-12 13:53:56 -0300

    Ajustes Header, com inclusão de novos elementos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import logo from '../img/logo.png';
 import './Header.css';
-import { FaSistrix } from 'react-icons/fa'
+import { FaSistrix } from 'react-icons/fa';
+import { BsFillCaretDownFill, BsPerson } from 'react-icons/bs';
+import { FiShoppingCart } from 'react-icons/fi';
 
 class Header extends React.Component {
   render() {
     return(
       <header>
-        <figure className="logo">
-          <img height="49" width="135" alt="logo da marca" src={logo} />
-        </figure>
-        <ul className="main-menu">
-          <li className="menu-item">Home</li>
-          <li className="menu-item">Categoria 1</li>
-          <li className="menu-item">Categiria 2</li>
-          <li className="menu-item">Contato</li>
-        </ul>
-        <div className="search-bar">
-          <input name="search-bar" className="search-bar-input" type="search" value="" placeholder="O que está procurando?" />
-          <FaSistrix />
+        <div className="header">
+          <figure className="logo">
+            <img height="49" width="135" alt="logo da marca" src={logo} />
+          </figure>
+          <ul className="main-menu">
+            <li className="menu-item">Home</li>
+            <li className="menu-item">Categoria 1 <BsFillCaretDownFill className="icon" /></li>
+            <li className="menu-item">Categoria 2 <BsFillCaretDownFill className="icon" /></li>
+            <li className="menu-item">Contato</li>
+          </ul>
+          <div className="search-bar">
+            <input name="search-bar" className="search-bar-input" type="search" value="" placeholder="O que está procurando?" />
+            <FaSistrix />
+          </div>
+          <span className="my-account"><BsPerson />Minha Conta</span>
+          <span className="cart"><FiShoppingCart />
+            <span className="cart-items"><p>1</p></span>
+          </span>
         </div>
       </header>
     )
