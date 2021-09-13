2bb62c717f8d35258c094d56d55289ef066cb3f6
Bruno Trindade <brunotrndd@gmail.com>
2021-09-13 19:24:18 -0300

    reposividade newsletter

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -5,7 +5,7 @@ class Newsletter extends React.Component {
   render() {
     return(
       <div className="newsletter">
-        <h2>Participe de nossas news com promoções e novidades!</h2>
+        <div className="news-text"><h2>Participe de nossas news com promoções e novidades!</h2></div>
         <form>
           <input placeholder="Digite seu nome"></input>
           <input placeholder="Digite seu email"></input>
